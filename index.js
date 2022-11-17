6ed3f8ac9870db659e49022ed7d1082c47916217
MatteoG <honrop.giochi@gmail.com>
2022-11-17 10:33:56 +0100

    Vers Base

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('datanasc').value = new Date().toISOString().slice(0, 10);
+
+let invio = document.getElementById('invia');
+invio.addEventListener('click', utente, false)
+
+function utente(){
+    console.log('Hai premuto invio')
+    document.getElementById('resultnome').textContent = document.getElementById('nome').value;
+    document.getElementById('resultcognome').textContent = document.getElementById('cognome').value;
+    document.getElementById('resultcf').textContent = document.getElementById('cf').value;
+    document.getElementById('resultdatanasc').textContent = document.getElementById('datanasc').value;
+    document.getElementById('resultindirizzo').textContent = document.getElementById('indirizzo').value;
+    document.getElementById('resultmail').textContent = document.getElementById('mail').value;
+    document.getElementById('resulttelefono').textContent = document.getElementById('telefono').value;
+    document.getElementById('resultoccupazione').textContent = document.getElementById('occupazione').value;
+}
