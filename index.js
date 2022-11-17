38ba309c47a6da2b9d42debc127a85401bc70d56
MatteoG <honrop.giochi@gmail.com>
2022-11-17 11:00:47 +0100

    impl controlli

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//formato corretto data:
 document.getElementById('datanasc').value = new Date().toISOString().slice(0, 10);
+
 
 let invio = document.getElementById('invia');
 invio.addEventListener('click', utente, false)
@@ -13,4 +15,10 @@ function utente(){
     document.getElementById('resultmail').textContent = document.getElementById('mail').value;
     document.getElementById('resulttelefono').textContent = document.getElementById('telefono').value;
     document.getElementById('resultoccupazione').textContent = document.getElementById('occupazione').value;
+}
+
+//controllo telefono
+let telefono = document.getElementById('telefono').value;
+if(telefono == ''){
+    alert('Inserisci un numero di telefono!')
 }
